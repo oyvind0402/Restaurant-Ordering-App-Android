eb4b200ca7c21db76efad1df338b6fc57771ea5b
oyvind0402 <56302124+oyvind0402@users.noreply.github.com>
2021-10-21 14:30:21 +0200

    Added some random styling for now for the frontpage and the save restaurant activity, also added toolbars and menu inflaters. Also added a settings icon and created a button for settings on the frontpage.

--- app/src/main/java/com/example/mappe2_s188886_s344046/FrontPageActivity.java
+++ app/src/main/java/com/example/mappe2_s188886_s344046/FrontPageActivity.java
@@ -1,8 +1,14 @@
 package com.example.mappe2_s188886_s344046;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 public class FrontPageActivity extends AppCompatActivity {
 
@@ -10,5 +16,40 @@ public class FrontPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frontpage_layout);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        myToolbar.inflateMenu(R.menu.front_menu);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.front_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.restaurant_activity:
+                Intent i = new Intent(this, RestaurantActivity.class);
+                startActivity(i);
+                break;
+            case R.id.friend_activity:
+                Intent i2 = new Intent(this, FriendActivity.class);
+                startActivity(i2);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+    public void visSettings(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        finish();
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/mappe2_s188886_s344046/RestaurantActivity.java
+++ app/src/main/java/com/example/mappe2_s188886_s344046/RestaurantActivity.java
@@ -1,4 +1,46 @@
 package com.example.mappe2_s188886_s344046;
 
-public class RestaurantActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class RestaurantActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.restaurant_layout);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        myToolbar.inflateMenu(R.menu.restaurant_menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.restaurant_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.frontpage_activity:
+                Intent i = new Intent(this, FrontPageActivity.class);
+                startActivity(i);
+                break;
+            case R.id.friend_activity:
+                Intent i2 = new Intent(this, FriendActivity.class);
+                startActivity(i2);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
 }
